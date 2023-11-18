7b172901a2195edcd1fcf84b8146579a7fb9113c
princetomar <madm47157@gmail.com>
2023-11-18 11:06:47 +0530

    Created basic student class and object of it

--- src/oops/StudentClass.java
+++ src/oops/StudentClass.java
@@ -1,7 +1,23 @@
 package oops;
 
 public class StudentClass {
+
+    // Create new datatype- Student class
+    public static class  Student{
+        String name;
+        int rollNo;
+        double percent;
+
+    }
     public static void main(String[] args){
-        System.out.println("Hello Prince");
+
+        // Create object of Student class
+        Student prince = new Student();
+        prince.name = "PRINCE TOMAR";
+        prince.rollNo = 120301;
+        prince.percent = 80.0;
+
+        System.out.println("Student name is : "+prince.name);
+
     }
 }
